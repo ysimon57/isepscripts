191af2c63a432a6b9a066d07b254a9436c861d26
yves2013 <dannyboygirl2010@gmail.com>
2013-04-17 16:12:36 +0300

    Update G1_find.sh

--- G1_find.sh
+++ G1_find.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
-file=
-
-if [ find . -name $1' = 1 ];then
-wc -l '$1'>result.txt
-cat result.txt
+#Afficher le nombre des lignes d'un fichier
+echo "Two arguments are required,one is for path,another is for a filename";
+if [ $# = 2 ];then
+path=$1 
+filename=$2
+file=`find $path -name '$2'`
+if [ -f $file ]; then
+nombre_ligne=`wc -l $file` 
+echo "File $2 has $nombre_ligne"
+else
+echo "can't find File!"
 fi
+else
+echo "Two arguments are required,one is for path,another is for a filename"
+fi
+
+

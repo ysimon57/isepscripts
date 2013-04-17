a016b87ab556720a05e08248a7d54463196fd832
spleen7 <enguerrand.preval@gmail.com>
2013-04-17 17:12:46 +0300

    Update lessG8.sh

--- lessG8.sh
+++ lessG8.sh
@@ -1,41 +1,54 @@
+                   SUMMARY OF LESS COMMANDS
 
-                   Résumé des commandes LESS
+      Commands marked with * may be preceded by a number, N.
+      Notes in parentheses indicate the behavior if N is given.
 
-      Les commandes marquées d'une étoile peuvent être précedées par un nombre.
-
-  h H affiche l'aide
-  q :q Q :Q ZZ à utiliser pour quitter l'affichage d'un fichier
+  h  H                 Display this help.
+  q  :q  Q  :Q  ZZ     Exit.
  ---------------------------------------------------------------------------
 
-                           Actions possibles
+                           MOVING
 
-  e ^E j ^N CR * Passer à la ligne suivante (ou N lignes).
-  y ^Y k ^K ^P * Revenir à la ligne précédente (ou N lignes).
-  f ^F ^V SPACE * Passer à la fenêtre suivante (ou plusieurs lignes).
-  b ^B ESC-v * Revenir à la fenêtre précédente (ou N lignes).
+  e  ^E  j  ^N  CR  *  Forward  one line   (or N lines).
+  y  ^Y  k  ^K  ^P  *  Backward one line   (or N lines).
+  f  ^F  ^V  SPACE  *  Forward  one window (or N lines).
+  b  ^B  ESC-v      *  Backward one window (or N lines).
+  z                 *  Forward  one window (and set window to N).
+  w                 *  Backward one window (and set window to N).
+  ESC-SPACE         *  Forward  one window, but don't stop at end-of-file.
+  d  ^D             *  Forward  one half-window (and set half-window to N).
+  u  ^U             *  Backward one half-window (and set half-window to N).
+  ESC-)  RightArrow *  Left  one half screen width (or N positions).
+  ESC-(  LeftArrow  *  Right one half screen width (or N positions).
+  F                    Forward forever; like "tail -f".
+  r  ^R  ^L            Repaint screen.
+  R                    Repaint screen, discarding buffered input.
         ---------------------------------------------------
         Default "window" is the screen height.
         Default "half-window" is half of the screen height.
  ---------------------------------------------------------------------------
 
-                          Recherche
+                          SEARCHING
 
-  /pattern * Recherche la ligne suivante correspondante.
-  ?pattern * Recherche la ligne précédente correspondant
-  n * répète la requête précédente.
-  N * répète la requête dans le sens opposé.
+  /pattern          *  Search forward for (N-th) matching line.
+  ?pattern          *  Search backward for (N-th) matching line.
+  n                 *  Repeat previous search (for N-th occurrence).
+  N                 *  Repeat previous search in reverse direction.
+  ESC-n             *  Repeat previous search, spanning files.
+  ESC-N             *  Repeat previous search, reverse dir. & spanning files.
+  ESC-u                Undo (toggle) search highlighting.
         ---------------------------------------------------
         Search patterns may be modified by one or more of:
-        ^N or ! Search for NON-matching lines.
-        ^E or * Search multiple files (pass thru END OF FILE).
-        ^F or @ Start search at FIRST file (for /) or last file (for ?).
-        ^K Highlight matches, but don't move (KEEP position).
-        ^R Don't use REGULAR EXPRESSIONS.
+        ^N or !  Search for NON-matching lines.
+        ^E or *  Search multiple files (pass thru END OF FILE).
+        ^F or @  Start search at FIRST file (for /) or last file (for ?).
+        ^K       Highlight matches, but don't move (KEEP position).
+        ^R       Don't use REGULAR EXPRESSIONS.
  ---------------------------------------------------------------------------
 
-                           Se déplacer dans un fichier
+                           JUMPING
 
-  g < ESC-< * Aller à la première ligne (ou la ligne N).
-  G > ESC-> * Aller à la dernière ligne (ou la ligne N).
-  p % * Aller au début du ichier (ou à N % du fichier).
-
+  g  <  ESC-<       *  Go to first line in file (or line N).
+  G  >  ESC->       *  Go to last line in file (or line N).
+  p  %              *  Go to beginning of file (or N percent into file).
+  t                 *  Go to the (N-th) next tag.

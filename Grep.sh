f32d86a3ab2a25dffcfbeba3aada9984f75e7a3b
louiseGaetan <louise.beleteau@gmail.com>
2013-04-17 15:50:28 +0300

    Update Grep.sh

--- Grep.sh
+++ Grep.sh
@@ -1,8 +1,22 @@
-grep "Une chaine de caractères"  NomFichier   # Afficher la ligne complète du fichier 
+# The command grep allows us to search a string in a file 
 
 
-grep -ANUM ":" test.txt       # afficher la ligne suivante 
-grep -B2 ":" test.txt       afficher les deux lignes précédente
+# show the lines which contain the searched string in a file
+grep "String"  FileName   
+
+# show the lines which contain the searched string in a file
+# And   the N next lines
+grep -AN "String"  FileName 
+
+# show the lines which contain the searched string in a file
+# And   the N previous lines
+grep -BN "String"  FileName     
+
+# show the lines which contain the searched string in a file,
+# the N previous lines and the N next lines
+grep -CN "String"  FileName
+
+
 grep -B1 ":" test.txt       afficher la ligne au dessus et au dessous de la chaine de   		           caractère recherche
 grep -b ":" test.txt      + affiche à le nombre caractère avant le texte 
 grep -n ":" test.txt		+ affiche le numéro de la ligne 

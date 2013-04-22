358f1f7e17c2811235995091fc5bfc2e6bee7cd5
kwispy <guigui7941@msn.com>
2013-04-22 14:56:47 +0300

    Update G1_find.sh

--- G1_find.sh
+++ G1_find.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #print the number of lines in a file 
 echo "Two arguments are required,one is for path,another is for a filename";
+echo ".";
 if [ $# = 2 ];then
 path=$1 
 filename=$2

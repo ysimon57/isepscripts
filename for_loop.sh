e63870eb7c7d87a29e9f9f33aa5d689e87c91fc1
Bastien <bzg@altern.org>
2013-04-22 14:50:16 +0300

    Add more for examples
    Example with various values.
    
    Example with a dumb counter.

--- for_loop.sh
+++ for_loop.sh
@@ -1,3 +1,13 @@
 for i in `ls *jpg`;
 do convert -resize 50% $i $i.png
 done
+
+for i in 1 2 3
+do
+  echo "i =" $i
+done
+
+for (( COUNTER=0; COUNTER <= 5; COUNTER++ ))
+do
+  echo "Add this line several times to a file" >> mytesfil.txt
+done

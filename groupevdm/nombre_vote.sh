f0abab922e00274fbf2ea3092e1064499c976b44
caroliner <caroline.romain@isep.fr>
2013-06-08 15:55:58 +0200

    Update nombre_vote.sh

--- groupevdm/nombre_vote.sh
+++ groupevdm/nombre_vote.sh
@@ -1,7 +1,18 @@
-#Ce code permet d'afficher le nombre de "Je valide, c'est une VDM" de la catégorie "argent" Pour cela, il faut d'abord se placer dans le répertoire de la catégorie "argent"
+#Ce code permet d'afficher le nombre de "Je valide, c'est une VDM" de la catégorie "argent" Pour cela, il faut d'abord se placer dans le répertoire de la catégorie "argent" ou "amour" au choix 
 
 #!/bin/sh
-cd www.viedemerde.fr/argent
+while true; do
+read -p "Argent ou amour ? " var # demande à l'utilisateur de choisir 
+  case $var in # test les entrées
+	argent) cd argent
+	echo "voic les résultats de la catégorie argent" 
+	break;; #arrete la boucle 
+	amour) cd amour 
+	break; # arrete la boucle
+	echo "voic les résultats de la catégorie amour";;
+*) echo "mauvaise entrée";; esac 
+done;
+
 dossier=`echo ${PWD##*/}`; #je garde le nom du dossier dans une variable
 for i in `ls` #je parcours tous les fichiers dans le dossier
 do
@@ -15,3 +26,5 @@ echo $tmp2
 
 done
 done
+ 
+# Kamaly SAMMANDAMOURTHY et caroline ROMAIN  
